883eb7b0b685918bef0b80d3315d464a08b1a697
khallaf <khallaftaha759@gmail.com>
2025-11-15 17:41:10 +0000

    add Log-cleaner.sh

---
+++ tools/Log-cleaner.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TARGET_DIR="/var/log"
+
+DAYS=7
+
+DRY_RUN=false
+
+if ["$DRY_RUN" = true]; then
+	echo "Dry run mode - files to be deleted:"
+	find "$TARGET_DIR" -type f -name "*.log" -mtime +$DAYS
+else
+	echo "Deleting log files older than $DAYS days in $TARGET_DIR"
+	find "$TAEGET_DIR" -type f -name "*.log" -mtime +$DAYS -exec rm -f {} \;
+	echo "Log cleanup completed."
+fi

edd8a24dc53ef29044dbbfd081a772a8cfed1cc3
khallaf <khallaftaha759@gmail.com>
2025-11-21 09:10:37 +0000

    Add auto-commit.sh

---
+++ tools/auto-commit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Requires inotify-tools: sudo apt install inotify-tool
+
+
+# FOLDER TO WATCH
+WATCH_DIR="./"
+
+
+# Loop to watch folders changes
+inoyifywait -m -r -e modify,create,delete --format '%w%f' "$WATCH_DIR" | while read FILE
+do
+	echo "Detected change in $FILE"
+
+	git add .
+	git commit -m " Auto commit: $(date '+%Y-%m-%d %H-%M-%S') -$FILE"
+	git push origin main
+
+	echo "Auto-commit completed for $FILE"
+
+done
